76cb0de7e458836e8fe83bd3ccfd8391224f605b
Vasyl Stokolosa <v.stokolosa@sysiq.com>
2014-06-30 17:52:46 +0300

    Optimisation code

--- js/backtotop.js
+++ js/backtotop.js
@@ -20,75 +20,90 @@
         var footer = $('#js-footer'),
             footerHeight,
             scrollToTop,
-            windowHeight;
+            windowHeight,
+            methods;
 
-        /**
-         * Click to Button
-         * @param coordinate - coordinate top of "content"
-         * @param time - time to animation
-         */
+        methods = $.extend({
 
-        function clickButton(element, coordinate, time) {
-            //check types
-            if (typeof coordinate !== 'number' || typeof time !== 'number') {
-                alert('ERROR \n\nUse number type for values \'coordinate\' and \'time\', please!')
-                return false;
-            }
-
-            //click to button
-            $(element).on('click', function () {
-                $('html,body').animate({
-                    scrollTop: coordinate
-                }, time );
-            });
-        };
-
-        /**
-         * FadeOut & FadeIn Button
-         *
-         */
-
-         function showButton(element) {
-
-            //scroll
-            $(window).scroll(function () {
-
-                scrollToTop = $(window).scrollTop();
-                windowHeight = $(window).height();
-
-                //fadeIn & fadeOut btn
-                if (scrollToTop >= windowHeight) {
-                    $(element).fadeIn();
-                } else if (scrollToTop <= windowHeight){
-                    $(element).fadeOut();
-                }
-
-                footerHeight = ( footer.length ) ? footer.offset().top : 0;
-
-                //check footer and add correct position for btn
-                if ( scrollToTop >= footerHeight - windowHeight ) {
-                    $(element).addClass('sticky').css({ top: footerHeight - 25 }); // 25 is a button height with space
-                } else {
-                    $(element).removeClass('sticky').css({ top: '' });
-                }
-            });
-        };
-
-        var methods = $.extend({
-            clickBtn: clickButton(this, 0, 400),
-            showBtn: showButton(this),
+            //values for .scrollTop() method
+            coordinate: 0,
+            time: 400,
 
             //css style
             left: 'auto',
             right: '20px',
             bottom: '10px'
-
         }, options);
 
-        return this.css ({
-            left: methods.left,
-            right: methods.right,
-            bottom: methods.bottom
+        return $(this).each(function () {
+
+            /**
+             * Click to Button
+             * @param coordinate - coordinate top of "content"
+             * @param time - time to animation
+             */
+
+            function clickButton(element) {
+                //check types
+                if (typeof methods.coordinate !== 'number' || typeof methods.time !== 'number') {
+                    alert('ERROR \n\nUse number type for values \'coordinate\' and \'time\', please!')
+                    console.log('Use number type for values \'coordinate\' and \'time\', please!')
+                    return false
+                }
+
+                //click to button
+                $(element).on('click', function () {
+                    $('html,body').animate({
+                        scrollTop: methods.coordinate
+                    }, methods.time );
+                });
+            }clickButton(this);
+
+
+            /**
+             * FadeOut & FadeIn Button
+             *
+             */
+
+            function showButton(element) {
+
+                //check if footer is :visible
+                if (footer.is(':visible')) {
+                } else {
+                    alert('ERROR \n\nFooter is not visible, please add id=\'js-footer\' for footer block');
+                    console.log('Footer is not visible, please add id=\'js-footer\' for footer block');
+                    return false;
+                };
+
+                //scroll
+                $(window).scroll(function () {
+                    scrollToTop = $(window).scrollTop();
+                    windowHeight = $(window).height();
+
+                    //fadeIn & fadeOut btn
+                    if (scrollToTop >= windowHeight) {
+                        $(element).fadeIn();
+                    } else if (scrollToTop <= windowHeight){
+                        $(element).fadeOut();
+                    }
+
+                    footerHeight = footer.offset().top;
+
+                    //check footer and add correct position for btn
+                    if ( scrollToTop >= footerHeight - windowHeight ) {
+                        $(element).addClass('sticky').css({ top: footerHeight - 25 }); // 25 is a button height with space
+                    } else {
+                        $(element).removeClass('sticky').css({ top: '' });
+                    }
+                });
+            }showButton(this);
+
+            //replace css properties
+            $(this).css ({
+                left: methods.left,
+                right: methods.right,
+                bottom: methods.bottom
+            });
         });
     };
 }(jQuery));
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 jQuery(function($) {
     $('#back').backToTop({
+        coordinate: 0,
+        time: 400,
         right: '15px'
     });
 });
